7fb45af0116e52be925085da143b029bd59acbd1
Peter Souter <petems@users.noreply.github.com>
2023-03-01 11:46:52 +0000

    Rebasing and updating `@actions/core`

--- index.test.js
+++ index.test.js
@@ -8,17 +8,11 @@ Name,Position,Wanted
 "Augustus, Victoria au",Accomplice,Yes
 `.trim();
 
-const expectedLog = `CSV Input given:
-Name,Position,Wanted
-"Andromedus, Darrow au",Leader,Yes
-"Augustus, Victoria au",Accomplice,Yes
-Markdown table Created:
+const expectedTable = `
 | Name                  | Position   | Wanted |
 | --------------------- | ---------- | ------ |
 | Andromedus, Darrow au | Leader     | Yes    |
 | Augustus, Victoria au | Accomplice | Yes    |
-
-::set-output name=markdown-table::| Name                  | Position   | Wanted |%0A| --------------------- | ---------- | ------ |%0A| Andromedus, Darrow au | Leader     | Yes    |%0A| Augustus, Victoria au | Accomplice | Yes    |
 `;
 
 // shows how the runner will run a javascript action with env / stdout protocol
@@ -26,5 +20,6 @@ test('parses csv and converts to valid markdown table', () => {
   process.env.INPUT_CSVINPUT = csvString;
   const markdownTable = path.join(__dirname, 'index.js');
   const actionLog = cp.execSync(`node ${markdownTable}`, { env: process.env }).toString();
-  expect(actionLog).toBe(expectedLog);
+  console.log(actionLog);
+  expect(actionLog).toMatch(expectedTable);
 });
